b71163a40c2f6895f150c86eb01838f213338218
teku <hernandezfredy8797@gmail.com>
2023-01-03 00:51:04 -0600

    update nvim files

--- nvim/lua/general.lua
+++ nvim/lua/general.lua
@@ -2,18 +2,25 @@
 
 local set = vim.opt -- set options
 
+-- tabs & indentation
 set.tabstop = 4
 set.softtabstop = 4
 set.shiftwidth = 4
+set.autoindent = true
 
 set.cursorline = true
 set.number = true
 
 set.mouse = 'a'
 
+-- search
 set.ignorecase = true
 set.hlsearch = true
+set.smartcase = true
 
-set.syntax = "ON"
+-- appearance
+set.syntax = "on"
 set.termguicolors = true
 
+-- clipboard
+set.clipboard:append("unnamedplus")
